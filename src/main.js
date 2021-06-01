852ccfcbdda46892bb9fd522f4506d8785e0c577
Jordon Leach <40806497+jordojordo@users.noreply.github.com>
2021-05-31 20:18:21 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import "~/assets/scss/main.scss";
 import DefaultLayout from '~/layouts/Default.vue'
 
