8259a9769114fb1d9ffd53beb5c41fb51505d546
jordonleach@gmail.com <jordonleach@gmail.com>
2021-06-02 10:13:33 -0400

    add jordo_com index page to redirect flow from old google search

---
+++ src/components/FullPageLoader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex h-screen">
+    <div class="m-auto">
+      <i
+        class="animate-spin fad fa-spinner-third fa-5x"
+        style="--fa-primary-color: #3881ff; --fa-secondary-opacity: .2;"
+      ></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FullPageLoader",
+};
+</script>
---
+++ src/pages/jordo_com/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <FullPageLoader />
+</template>
+
+<script>
+import FullPageLoader from "@/components/FullPageLoader";
+
+export default {
+  components: {
+    FullPageLoader
+  },
+  mounted() {
+    window.location.href = "https://jordonleach.dev/";
+  },
+};
+</script>
